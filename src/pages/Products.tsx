787ab6f2d80bc1b429e3b47cd2bea9ecdd93426b
YannExdi <115077840+yann1945@users.noreply.github.com>
2026-01-29 21:25:41 +0700

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -10,7 +10,6 @@ import product2 from "@/assets/product-2.jpg";
 import product3 from "@/assets/product-3.jpg";
 import product4 from "@/assets/product-4.jpg";
 import product5 from "@/assets/product-5.jpg";
-import product6 from "@/assets/product-6.jpg";
 
 const products = [
   {
