434071197ca7b89c4451a2c47dd33e82828a98dd
YannExdi <115077840+yann1945@users.noreply.github.com>
2026-02-02 13:32:26 +0700

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -167,7 +167,7 @@ const Products = () => {
 
                     <div className="mt-4 flex items-center justify-between">
                       <span className="font-serif text-2xl font-bold text-primary">
-                        Rp.{product.price}
+                        Rp. {product.price}
                       </span>
                       <Button size="sm" className="gap-2">
                         <ShoppingBag size={16} />
